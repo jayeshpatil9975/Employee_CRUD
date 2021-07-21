4f57a1acc0a19eaa7e6b05a7394c6b49201c4206
jayeshpatil9975 <87759168+jayeshpatil9975@users.noreply.github.com>
2021-07-21 16:26:41 +0530

    Add files via upload

---
+++ EmployeeCrud/EmployeeCrud/Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCrud.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions opts) : base(opts)
+        { }
+        public DbSet<Employee> Employees { get; set; }
+        
+    }
+}
+
